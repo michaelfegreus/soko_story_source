cc92aba173f0fb77177fefef1dcdfe65c5c20fc4
Michael Fegreus <michael.fegreus@gmail.com>
2017-11-13 09:15:05 -0800

    Closing in on end of demo. Need to get ready for Megabooth entrance!

---
+++ Assets/Scripts/Player/mono_ground_check.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mono_ground_check : MonoBehaviour {
+
+	Rigidbody rb;
+	RaycastHit hit;
+
+	public float heightAdjust;
+	public float myGravity;
+
+	public bool onGround;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Checks to see if you're on the ground and not falling currently.
+		if (rb.velocity.y < 0.05f && Vector3.Distance (transform.position, hit.point) <= heightAdjust) {
+			onGround = true;
+			transform.position = new Vector3 (transform.position.x, hit.point.y + heightAdjust, transform.position.z);
+			// Ground drag back on
+			//Debug.Log("On Ground reset accessed");
+		} else if (Vector3.Distance (transform.position, hit.point) > heightAdjust + 0.05f) {
+			onGround = false;
+		}
+	}
+
+	void FixedUpdate(){
+
+		Ray groundRay = new Ray (transform.position, -transform.up);
+		if (Physics.Raycast (groundRay, out hit, 1000f)) {
+			// Sets you to heightAdjust's distance above the ground.
+		}
+
+		// Always apply significant gravity if not on ground
+		if (!onGround) {
+			rb.AddForce (-transform.up * myGravity, ForceMode.Impulse);
+		}
+
+		//Debug.Log (hit.point);
+		Debug.DrawLine(transform.position, hit.point, Color.red);
+	}
+}
---
+++ Assets/Scripts/Player/mono_player_control.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mono_player_control : MonoBehaviour {
+
+	float inputX = 0f;
+	float inputY = 0f;
+
+	float targetMoveSpeed;
+	public float baseMoveSpeed;
+	public float accelerationRate;
+	public float deccelerationRate;
+	float currentMoveSpeed = 0f;
+
+	Rigidbody rb;
+
+	//public bool onGround = true;
+
+	Vector3 movement;
+
+	void Start(){
+		targetMoveSpeed = baseMoveSpeed;
+
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+		inputX = Input.GetAxis("Horizontal");
+		inputY = Input.GetAxis("Vertical");
+
+		movement = new Vector3 (inputX, 0.0f, inputY);
+
+		transform.rotation = Quaternion.LookRotation(movement);
+
+		Debug.Log ("Current Move Speed: " + currentMoveSpeed);
+
+	}
+
+	void FixedUpdate (){
+
+	
+
+		/*Ray groundRay = new Ray(transform.position, -transform.up);
+		if (Physics.Raycast(groundRay, out hit, 1000f)) {
+			// Sets you to heightAdjust's distance above the ground.
+		}*/
+
+
+		// Move input that pushes the character forward towards the direction faced
+		if (inputX < -.001f || inputX > .001f || inputY > .001f || inputY < -.001f) {
+			targetMoveSpeed = baseMoveSpeed;
+			if (currentMoveSpeed < targetMoveSpeed) { // Must accelerate.
+				currentMoveSpeed = Mathf.Lerp (currentMoveSpeed, targetMoveSpeed, Time.deltaTime * accelerationRate);
+			}
+			// Apply force to begin moving!
+			rb.AddForce (transform.forward * currentMoveSpeed, ForceMode.Impulse);
+		} else {
+			/*if (currentMoveSpeed > 0f) { // Must deccelerate.
+				currentMoveSpeed = 0f;
+				rb.velocity = new Vector3 (0f, 0f, 0f);
+			}*/
+			currentMoveSpeed = 0f;
+			rb.velocity = new Vector3 (0f, 0f);
+
+		}
+	
+	}
+}
